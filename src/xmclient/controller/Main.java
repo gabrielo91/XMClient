914fbc77eef45d70770de335520134a5375b4dcd
gabrielo91 <gabrielo_9103@hotmail.com>
2017-11-28 23:11:42 -0500

    Main in pdn mode

--- src/xmclient/controller/Main.java
+++ src/xmclient/controller/Main.java
@@ -25,13 +25,7 @@ import xmclient.soapentities.ReportReadingProcessResult;
  */
 public class Main {
     public static void main(String[] args) {
-    
-        System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
-        System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
-        System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
-        System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
-        
-        
+
         try {
             
             //Testing -----------------------------------------------
